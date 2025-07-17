cb74a0d77a3363618e5c00ff563326bb42576ca5
yaomingxinlu-local <zangqing1990@gmail.com>
2025-07-16 18:49:37 -0700

    first commit

---
+++ src/main/java/com/example/ExpenseTracker_server_JavaSpringboot_qz/ExpenseTrackerServerJavaSpringbootQzApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExpenseTracker_server_JavaSpringboot_qz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerServerJavaSpringbootQzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerServerJavaSpringbootQzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExpenseTracker_server_JavaSpringboot_qz/ExpenseTrackerServerJavaSpringbootQzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExpenseTracker_server_JavaSpringboot_qz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerServerJavaSpringbootQzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
