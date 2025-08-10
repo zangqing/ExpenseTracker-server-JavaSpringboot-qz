deb8116d8110798037f3c849268bcbe14a8b1639
yaomingxinlu-local <zangqing1990@gmail.com>
2025-08-10 14:33:34 -0700

    enable Cross-Origin Resource Sharing (CORS) and handle preflight request

--- src/main/java/com/example/ExpenseTracker_server_JavaSpringboot_qz/config/SpringSecurityConfig.java
+++ src/main/java/com/example/ExpenseTracker_server_JavaSpringboot_qz/config/SpringSecurityConfig.java
@@ -40,6 +40,7 @@ public class SpringSecurityConfig {
 //                    authorize.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "USER");
 //                    authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll();
                     authorize.requestMatchers("/api/auth/**").permitAll();
+                    authorize.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
                     authorize.anyRequest().authenticated();
                 }).httpBasic(Customizer.withDefaults());
 
